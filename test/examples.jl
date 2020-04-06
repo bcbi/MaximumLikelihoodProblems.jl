433545537ca43fd31d2d6eb39dba549f690f1fe0
Dilum Aluthge <dilum@aluthge.com>
2020-04-06 09:04:10 -0400

    Clarify, reorganize, and simplify examples

--- test/examples.jl
+++ test/examples.jl
@@ -4,29 +4,20 @@ import Test
 Test.@testset "examples" begin
     Test.@testset "linear regression" begin
         Random.seed!(123)
-        include(joinpath(examples_directory,
-                         "linear_regression.jl"))
-        include(joinpath(test_directory,
-                         "examples",
-                         "linear_regression.jl"))
+        include(joinpath(examples_directory, "linear_regression.jl"))
+        include(joinpath(test_directory, "examples", "linear_regression.jl"))
 
     end
 
     Test.@testset "logistic regression" begin
         Random.seed!(123)
-        include(joinpath(examples_directory,
-                         "logistic_regression.jl"))
-        include(joinpath(test_directory,
-                         "examples",
-                         "logistic_regression.jl"))
+        include(joinpath(examples_directory, "logistic_regression.jl"))
+        include(joinpath(test_directory, "examples", "logistic_regression.jl"))
     end
 
     Test.@testset "multinomial logistic regression" begin
         Random.seed!(123)
-        include(joinpath(examples_directory,
-                         "multinomial_logistic_regression.jl"))
-        include(joinpath(test_directory,
-                         "examples",
-                         "multinomial_logistic_regression.jl"))
+        include(joinpath(examples_directory, "multinomial_logistic_regression.jl"))
+        include(joinpath(test_directory, "examples", "multinomial_logistic_regression.jl"))
     end
 end
