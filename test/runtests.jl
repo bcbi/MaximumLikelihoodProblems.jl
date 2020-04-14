9d4389b771a49e2fe5240ca9ac373441b6f22e15
Dilum Aluthge <dilum@aluthge.com>
2020-04-14 22:43:24 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,5 +11,14 @@ examples_directory = joinpath(root_directory, "examples")
 test_directory = joinpath(root_directory, "test")
 
 Test.@testset "MaximumLikelihoodProblems.jl" begin
-    include("examples.jl")
+    Test.@testset "linear_algebra.jl" begin
+        Test.@testset "_pseudoinverse" begin
+            Test.@test _pseudoinverse([1 2; 3 4]) == LinearAlgebra.inv([1 2; 3 4])
+            Test.@test _pseudoinverse([0 0; 0 0]) == [0 0; 0 0]
+        end
+    end
+    
+    Test.@testset "examples" begin
+        include("examples.jl")
+    end
 end
